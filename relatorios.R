cd55a491e84a877713bc95d897e0a2eecb81780c
Tomás de Siervi Barcellos <tomas.barcellos@agricultura.gov.br>
2019-10-17 10:16:25 -0300

    versão dos textos sem espaços duplicados

--- relatorios.R
+++ relatorios.R
@@ -1,11 +1,21 @@
 library(pdftools)
+library(tidyverse)
+
+compactar <- function(str) {
+  str %>% 
+    str_split("\\r?\\n") %>% 
+    unlist()
+}
 
 arquivos <- dir("pdf/", full.names = TRUE)
-
-relatorios <- purrr::map(arquivos, pdf_text)
 destinos <- paste0("txt/ra", 12:18, ".txt")
 
-purrr::walk2(relatorios, destinos, readr::write_lines)
+relatorios <- map(arquivos, pdf_text)
+relatorios_limpo <- map(relatorios, compactar) %>% 
+  map(str_squish) %>% 
+  map(str_trim)
+
+walk2(relatorios_limpo, destinos, readr::write_lines)
 
 
 
